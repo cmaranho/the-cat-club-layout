9e177cfb2d90c54353f8acabb1c8a716e030430a
César Maranho <cesar.maranho@hotmail.com>
2018-06-20 19:10:24 -0300

    Update code
    Update code

---
+++ js/initMain.js
@@ -0,0 +1,1 @@
+var initMainJS = new MainClass();
--- js/main.js
+++ js/main.js
@@ -1,10 +1,37 @@
 scrollFN = () => {
-    window.onscroll = () => {
-        const nav = document.querySelector('nav');
-        if(this.scrollY <= 10) nav.className = 'navColor--transparent'; else nav.className = 'navColor';
-      };
-  }
+  window.onscroll = () => {
+    const nav = document.querySelector('nav');
+    if (this.scrollY <= 10) nav.className = 'navColor--transparent'; else nav.className = 'navColor';
+  };
+}
 
-  scrollFN();
+initMap = () => {
+  var locationMap = { lat: -23.5704701, lng: -46.6462687 };
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: locationMap
+  });
 
-  console.log(scrollFN());
+  var contentString = `
+        <div id="content">  
+          <div id="bodyContent">
+            <p><b>Cat Club</b>, this is cat club, the best place for cute cats!</p>
+          </div>
+        </div> `;
+
+  var infowindow = new google.maps.InfoWindow({
+    content: contentString
+  });
+  var marker = new google.maps.Marker({
+    position: locationMap,
+    map: map,
+    title: 'The cat Club'
+  });
+  marker.addListener('click', function () {
+    infowindow.open(map, marker);
+  });
+}
+
+scrollFN()
+
+
